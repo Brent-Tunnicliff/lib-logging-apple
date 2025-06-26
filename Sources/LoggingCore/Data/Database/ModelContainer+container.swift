c9aa65b775360838e395e1679e758b458e17aa5a
Brent Tunnicliff <brent@tunnicliff.dev>
2025-06-26 23:32:35 +1000

    Moved View logic from LoggingCore

--- Sources/LoggingCore/Data/Database/ModelContainer+container.swift
+++ Sources/LoggingCore/Data/Database/ModelContainer+container.swift
@@ -2,10 +2,9 @@
 
 import Foundation
 package import SwiftData
-package import SwiftUI
 
 extension ModelContainer {
-    static let shared: ModelContainer = {
+    package static let shared: ModelContainer = {
         do {
             let schema = Schema(versionedSchema: LatestSchema.self)
             // The configuration needs a name so it does not get applied to the app's default database.
@@ -21,43 +20,8 @@ extension ModelContainer {
     }()
 }
 
-extension EnvironmentValues {
-    /// Model container used by logging views.
-    @Entry package var loggingModelContainer: ModelContainer = .shared
-}
-
 #if DEBUG
     // MARK: - Mock
-
-    /// Represents how to populate the mocked logging container.
-    package struct MockedLoggingModelContainerState: Sendable {
-        /// Container to be created empty.
-        package static let empty = MockedLoggingModelContainerState(wrapped: .empty)
-
-        /// Container to be populated with a variety of entities.
-        package static let populated = MockedLoggingModelContainerState(wrapped: .populated)
-
-        let wrapped: Wrapped
-
-        enum Wrapped: Sendable {
-            case empty
-            case populated
-        }
-    }
-
-    extension View {
-        /// Replaces the logging model container with a mocked in memory instance.
-        /// - Parameters:
-        ///   - state: State of the mock to be populated.
-        ///   - name: Name to be used as part of the container. Defaults to the function that calls this.
-        /// - Returns: self with the injected model container.
-        package func loggingModelContainer(
-            mocked state: MockedLoggingModelContainerState,
-            name: String = #function
-        ) -> some View {
-            environment(\.loggingModelContainer, .emptyInMemoryOnly(name: name).injectingMocks(logs: state.entities))
-        }
-    }
 
     extension ModelContainer {
         @MainActor
@@ -78,21 +42,12 @@ extension EnvironmentValues {
         }
 
         @MainActor
-        func injectingMocks(logs: [LogEntity] = LogEntity.defaultMocks()) -> ModelContainer {
+        package func injectingMocks(logs: [LogEntity] = LogEntity.defaultMocks()) -> ModelContainer {
             for log in logs {
                 mainContext.insert(log)
             }
 
             return self
-        }
-    }
-
-    extension MockedLoggingModelContainerState {
-        var entities: [LogEntity] {
-            switch wrapped {
-            case .empty: []
-            case .populated: LogEntity.defaultMocks()
-            }
         }
     }
 #endif
---
+++ Sources/LoggingUI/Helper/View+loggingModelContainer.swift
@@ -0,0 +1,54 @@
+// Copyright © 2025 Brent Tunnicliff <brent@tunnicliff.dev>
+
+import LoggingCore
+public import SwiftData
+public import SwiftUI
+
+extension EnvironmentValues {
+    /// Model container used by logging views.
+    @Entry public var loggingModelContainer: ModelContainer = .shared
+}
+
+#if DEBUG
+
+    // MARK: - Mock
+
+    /// Represents how to populate the mocked logging container.
+    public struct MockedLoggingModelContainerState: Sendable {
+        /// Container to be created empty.
+        public static let empty = MockedLoggingModelContainerState(wrapped: .empty)
+
+        /// Container to be populated with a variety of entities.
+        public static let populated = MockedLoggingModelContainerState(wrapped: .populated)
+
+        fileprivate let wrapped: Wrapped
+
+        fileprivate enum Wrapped: Sendable {
+            case empty
+            case populated
+        }
+    }
+
+    extension View {
+        /// Replaces the logging model container with a mocked in memory instance.
+        /// - Parameters:
+        ///   - state: State of the mock to be populated.
+        ///   - name: Name to be used as part of the container. Defaults to the function that calls this.
+        /// - Returns: self with the injected model container.
+        public func loggingModelContainer(
+            mocked state: MockedLoggingModelContainerState,
+            name: String = #function
+        ) -> some View {
+            environment(\.loggingModelContainer, .emptyInMemoryOnly(name: name).injectingMocks(logs: state.entities))
+        }
+    }
+
+    extension MockedLoggingModelContainerState {
+        fileprivate var entities: [LogEntity] {
+            switch wrapped {
+            case .empty: []
+            case .populated: LogEntity.defaultMocks()
+            }
+        }
+    }
+#endif
