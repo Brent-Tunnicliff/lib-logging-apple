a0ac18a28d45a77cb59c89f32c74479e9c133f41
Brent Tunnicliff <brent@tunnicliff.dev>
2025-07-10 22:13:23 +1000

    Added presentationDragIndicator to logs view sheet.

--- Sources/LoggingUI/LogsViewToolbarItem.swift
+++ Sources/LoggingUI/LogsViewToolbarItem.swift
@@ -50,6 +50,7 @@ public struct LogsViewToolbarItem: ToolbarContent {
                             }
                         }
                 }
+                .presentationDragIndicator(.visible)
             }
         }
     }
