af83442991a48a69c6c5e9e5fddc8d4ac88fc86e
Brent Tunnicliff <brent@tunnicliff.dev>
2025-10-11 23:29:59 +1100

    Additional attempt to resolve flaky test.

--- Tests/LoggingCoreTests/Data/NotificationProviderTests.swift
+++ Tests/LoggingCoreTests/Data/NotificationProviderTests.swift
@@ -1,13 +1,14 @@
 // Copyright © 2025 Brent Tunnicliff <brent@tunnicliff.dev>
 
 import Foundation
+import Synchronization
 import Testing
 
 @testable import LoggingCore
 
 @MainActor
 struct NotificationProviderTests {
-    private let notificationCenter = NotificationCenter.default
+    private let notificationCenter = WrappedNotificationCenter()
     private let notificationName = Notification.Name(rawValue: "DefaultNotificationProviderTestsMock")
     private let notificationProvider: DefaultNotificationProvider
 
@@ -31,11 +32,36 @@ struct NotificationProviderTests {
             return false
         }
 
-        while setupIsReady == false {
+        while !setupIsReady || !notificationCenter.addObserverCalled.load(ordering: .sequentiallyConsistent) {
             try await Task.sleep(for: .milliseconds(10))
         }
 
         notificationCenter.post(name: notificationName, object: nil)
         await #expect(notificationTriggered.value == true)
+    }
+}
+
+private final class WrappedNotificationCenter: NotificationCenter, @unchecked Sendable {
+    let addObserverCalled = Atomic(false)
+
+    override func addObserver(
+        _ observer: Any,
+        selector aSelector: Selector,
+        name aName: NSNotification.Name?,
+        object anObject: Any?
+    ) {
+        super.addObserver(observer, selector: aSelector, name: aName, object: anObject)
+        addObserverCalled.store(true, ordering: .sequentiallyConsistent)
+    }
+
+    override func addObserver(
+        forName name: NSNotification.Name?,
+        object obj: Any?,
+        queue: OperationQueue?,
+        using block: @escaping @Sendable (Notification) -> Void
+    ) -> any NSObjectProtocol {
+        let result = super.addObserver(forName: name, object: obj, queue: queue, using: block)
+        addObserverCalled.store(true, ordering: .sequentiallyConsistent)
+        return result
     }
 }
